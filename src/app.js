9a889e6393b1cdc58c147fef72e74ed74d6bd755
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2025-10-19 20:10:42 +0530

    controller and route configured for registering the user

--- src/app.js
+++ src/app.js
@@ -9,15 +9,18 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
-
 app.use(express.json({
     limit:"16KB"
 }))
 app.use(express.urlencoded())
-
 app.use(cookieParser())
-
 app.use(express.static("public"))
+
+import userRouter from './routes/user.routes.js'
+app.use("/api/v1/user",userRouter)
+
+
+
 
 
 export {app}
