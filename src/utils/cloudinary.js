dfcb80577e68ae64a2e4bd50a83502edff1a11ba
satyam-kumar-dubey <shouryabhardwaj572@gmail.com>
2025-10-19 23:40:02 +0530

    File unlinked upon successful upload on cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,7 +17,8 @@ const uploadOnCloudinary = async (localFilePath)=> {
             resource_type: "auto",
         })
         //file has been successfully uploaded
-        console.log("file is successfully uploaded", response.url);
+        // console.log("file is successfully uploaded", response.url);
+        fs.unlinkSync(localFilePath)
         return response
         
     } catch (error) {
